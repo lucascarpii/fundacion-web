856b63db022f0c71a6a7c0617ae9c7dd4cba81c8
Lucas Fuentes <lucascarpii2002@gmail.com>
2025-04-15 19:07:46 -0300

    Add Banner component with live stream functionality; update Hero component styles and integrate runCode hook

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -4,14 +4,14 @@ import { SearchIcon } from "../../icons/SearchIcon";
 
 export function Hero() {
   return (
-    <main className="w-full min-h-dvh bg-hero-image bg-no-repeat bg-cover bg-center flex items-center relative px-5 lg:px-20 lg:pt-10">
-      <div className="absolute w-full left-0 bottom-8 flex justify-center z-10">
+    <main className="w-full min-h-[80dvh] bg-hero-image bg-no-repeat bg-cover bg-center flex items-center relative px-5 lg:px-20 lg:pt-10">
+      {/* <div className="absolute w-full left-0 bottom-8 flex justify-center z-10">
         <div className="max-w-screen-xl mx-auto w-full flex justify-end px-6">
           <a href="#areas" className="flex cursor-pointer items-center gap-2 tracking-wide font-light text-white">
             Explorar más <span className="bg-white text-black rounded-full p-1 rotate-90"> <ArrowRightIcon className="size-5" /></span>
           </a>
         </div>
-      </div>
+      </div> */}
 
       <div id="overlay" className="bg-isei-footer/60 absolute inset-0"></div>
       <section className="max-w-screen-xl flex justify-center items-center lg:items-start mx-auto text-center flex-col lg:text-start w-full z-40">
